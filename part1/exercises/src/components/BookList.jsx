0433e1b4958d397d754f6c9a89c2acb5a0f5ce75
HenryLuepke <henryluepke4@gmail.com>
2024-08-19 16:01:02 -0500

    React part 1 exercise

--- part1/exercises/src/components/BookList.jsx
+++ part1/exercises/src/components/BookList.jsx
@@ -1,15 +1,15 @@
 export default function BookList() {
-   let pageTitle = "shrug";
-   let book1 = "google.com";
-   let book2 = "";
-   let book3 = "";
+   let pageTitle = "Book List";
+   let book1 = "https://images4.penguinrandomhouse.com/cover/9780593801970";
+   let book2 = "https://images4.penguinrandomhouse.com/cover/9780593230381";
+   let book3 = "https://images1.penguinrandomhouse.com/cover/9780385547994";
 
    return (
       <div>
          <h3>{pageTitle}</h3>
-         <img src={book1} alt="Nothing here!" />
-         <img src={book2} alt="Nothing here!" />
-         <img src={book3} alt="Nothing here!" />
+         <img src={book1} alt="The City and Its Uncertain Walls by Haruki Murakami" />
+         <img src={book2} alt="The Message by Ta-Nehisi Coates" />
+         <img src={book3} alt="Death at the Sign of the Rook by Kate Atkinson" />
       </div>      
    );
 }
